0d3443a0224064b7cb99e5c19757a16932d5f0ac
alam1906 <107329748+alam1906@users.noreply.github.com>
2025-04-04 09:10:26 -0700

    10

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({
-			strict: false,
-			fallback: 'index.html'
-		})
+		adapter: adapter(),
+		prerender: {
+			entries: ['*'] // Pastikan semua halaman diprerender
+		}
 	}
 };
