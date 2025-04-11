7ad93d06d8030fc88cd78e880d7b025c10cba10c
alam1906 <107329748+alam1906@users.noreply.github.com>
2025-04-11 05:17:25 -0700

    berhasil

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
-		prerender: {
-			entries: ['*', '/', '/portofolio', '/certification']
-		},
-		adapter: adapter({ fallback: 'index.html' })
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
